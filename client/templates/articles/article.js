7b728f787cb516d248ce8b663b8e3e6919ed37a9
Sumit Sarkar <mailsumit.sarkar@gmail.com>
2015-08-03 11:25:25 +0100

    Added Article styles. ToDo- Fix syntax highlighting on page load.

--- client/templates/articles/article.js
+++ client/templates/articles/article.js
@@ -2,15 +2,40 @@ Template.article.helpers({
 	getArticle: function() {
 		//var feedId = FlowRouter.params('feedId');
 		var articleId = FlowRouter.getParam('articleId')
-		return FeedsArticles.findOne({_id: articleId}, {sort: {date: -1}});
+		return FeedsArticles.findOne({
+			_id: articleId
+		}, {
+			sort: {
+				date: -1
+			}
+		});
 	},
 
 	getDateString: function(date) {
 		return moment(date).format("Do MMM YY");
+	},
+
+	postProcessing: function() {
+
 	}
 });
 
 Template.article.onRendered(function() {
 	var x = document.getElementsByTagName('*');
 	removeStyle(x);
+
+	var pre = $("pre");
+	var code = $("code");
+	if (!pre.hasClass('prettyprint'))
+		pre.addClass('prettyprint');
+
+	if (!code.hasClass('prettyprint'))
+		code.addClass('prettyprint');
+ 		
+	if (prettyPrint)
+		prettyPrint();
+});
+
+Template.article.events({
+	
 });
